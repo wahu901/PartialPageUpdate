71d42b721a4ba6bf61a1cd0aa0ebfaac767b1d80
Wayne Hu <wahu901@gmail.com>
2020-03-06 11:42:24 -0800

    Add project files.

---
+++ Pages/AjaxPartial.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PartialPageUpdate.Models;
+using PartialPageUpdate.Services;
+
+namespace PartialPageUpdate
+{
+    public class AjaxPartialModel : PageModel
+    {
+        private CarService _carService;
+        public AjaxPartialModel(CarService carService)
+        {
+            _carService = carService;
+        }
+
+        public List<Car> Cars { get; set; }
+        public void OnGet()
+        {
+            Cars = _carService.GetAll();
+        }
+
+        
+        public PartialViewResult OnGetCarPartial()
+        {
+            Cars = _carService.GetAll();
+            return Partial("_CarPartial", Cars);
+        }
+
+
+      
+    }
+}
